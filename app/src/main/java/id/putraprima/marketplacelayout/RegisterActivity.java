409620039294934ff4b7287b1e343af79c399582
samsul24 <samsulibs30@gmail.com>
2020-02-16 14:19:47 +0700

    Menyelesaikan

--- app/src/main/java/id/putraprima/marketplacelayout/RegisterActivity.java
+++ app/src/main/java/id/putraprima/marketplacelayout/RegisterActivity.java
@@ -1,23 +1,20 @@
 package id.putraprima.marketplacelayout;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_register);
     }
-    public void clickMasuk(View view) {
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-            startActivity(intent);
+    public void clickKembaliLogin(View view) {
+        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(intent);
         finish();
     }
 }
